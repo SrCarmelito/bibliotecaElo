feb513e7f155643f6779ff576538f57885aefc9b
Carmelito Jr <carmelito.benali@hotmail.com>
2025-11-28 08:40:51 -0300

    RefatoracoesGerais

--- server/src/main/java/com/bibliotecaelo/utils/AuditUserUtil.java
+++ server/src/main/java/com/bibliotecaelo/utils/AuditUserUtil.java
@@ -4,31 +4,27 @@ import java.util.Objects;
 import java.util.UUID;
 
 import com.bibliotecaelo.domain.Usuario;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuditUserUtil {
 
     public Usuario getAuditUser() {
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(Objects.nonNull(authentication)) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof Usuario usuario) {
+                return usuario;
+            }
+        }
+
         final Usuario anonymousUser = new Usuario();
         anonymousUser.setId(UUID.fromString("e25e251b-f4ba-42b9-9861-2bde06584daf"));
         anonymousUser.setNome("anonymousUser");
         anonymousUser.setLogin("anonymousUser");
 
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        if (Objects.isNull(authentication) || authentication instanceof AnonymousAuthenticationToken) {
-            return anonymousUser;
-        } else {
-
-            Object principal = authentication.getPrincipal();
-
-            if (principal instanceof Usuario usuario) {
-                return usuario;
-            } else {
-                return anonymousUser;
-            }
-        }
+        return anonymousUser;
     }
 }
